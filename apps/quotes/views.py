f29a0066cf2b854a34edbcdba4d9d195b2ccf3c0
nicolasventin <128478771+nicolasventin@users.noreply.github.com>
2026-08-29 15:49:36 -0300

    Presupuestos: habilitar vistas de edición

--- apps/quotes/views.py
+++ apps/quotes/views.py
@@ -8,7 +8,7 @@ from django.shortcuts import get_object_or_404, redirect, render
 from django.template.loader import render_to_string
 from django.urls import reverse
 from django.views import View
-from django.views.generic import CreateView, DetailView, ListView
+from django.views.generic import CreateView, DetailView, ListView, UpdateView
 
 from apps.audit.services import log_action
 from apps.catalog.models import ProductoProveedor
